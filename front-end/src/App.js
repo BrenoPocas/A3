029f7ec62a23e808ed1d741ed6f5918f4d19ac29
Lorrayne <lorrayne.santana2022@gmail.com>
2024-09-27 19:58:19 -0300

    começo politica e privacidade e termos de uso

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import footer from './components/footer';
 import header from './components/header';
+import termos from './pages/termos';
+import politicas from './pages/politica-privacidade';
 
 function App() {
   return (
     <div className="App">
       {header()}
+      {termos()}
+      {politicas()}
       {footer()}
     </div>
   );
