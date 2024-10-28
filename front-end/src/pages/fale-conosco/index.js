79fea3aea7c188f3d990731dd31bfda99f32e5e2
Lorrayne <lorrayne.santana2022@gmail.com>
2024-10-28 20:40:05 -0300

    formularios fale conosco

--- front-end/src/pages/fale-conosco/index.js
+++ front-end/src/pages/fale-conosco/index.js
@@ -2,15 +2,37 @@ import './style.css'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
 
-function ContaVerificada() {
+function FaleConosco() {
 
     return <div>
+        
         <Header/>
+
         <section>
-            <h2 className='titulo...'>Fale Conosco</h2>
+
+        <form className="formulario_criar_conta" action="#">
+
+            <label for="nomeCompleto">Nome Completo:</label>
+            <input type="text" id="nomeCompleto" required/>
+
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" required/>
+
+            <label for="assunto">Assunto:</label>
+            <input type="text" id="assunto" required/>
+
+            <label for="mensagem">Sua mensagem:</label>
+            <textarea type="text" id="mensagem" required></textarea>                
+
+            <button className="botao_enviar_contato" type="button">Enviar Mensagem</button>
+
+        </form>
+
         </section>
+
         <Footer/>
+
     </div>;
 }
 
-export default ContaVerificada
+export default FaleConosco
