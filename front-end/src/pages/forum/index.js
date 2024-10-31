977a9e84b114f7cf93239d8daac6605ae8ac2e73
Marcello Júnior <celojuniorii2003@gmail.com>
2024-10-31 15:12:05 -0300

    Em_Construção_Forum

--- front-end/src/pages/forum/index.js
+++ front-end/src/pages/forum/index.js
@@ -2,12 +2,16 @@ import './style.css'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
 
+import IMAGE from './img/em_contrucao.png'
+
 function Forum() {
 
     return <div>
         <Header/>
         <section>
-            <h2 className='titulo...'>Forum</h2>
+            <h2 className='titulo...'>Forum</h2>    
+            <img src={IMAGE} alt="Em construção" />
+            
         </section>
         <Footer/>
     </div>;
