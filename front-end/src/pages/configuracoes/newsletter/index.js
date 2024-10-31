bd42b019dcc97dee861692b5697435f3e7680e81
Marcello Júnior <celojuniorii2003@gmail.com>
2024-10-31 15:42:27 -0300

    correção

--- front-end/src/pages/configuracoes/newsletter/index.js
+++ front-end/src/pages/configuracoes/newsletter/index.js
@@ -2,6 +2,7 @@ import './style.css'
 import Header from '../../../components/header'
 import Footer from '../../../components/footer'
 import ItensConfiguracoes from '../../../components/itens-configuracoes';
+import IMAGEN from './img/em_contrucao1.png'
 
 function Newsletter() {
 
@@ -16,6 +17,7 @@ function Newsletter() {
                 <div className="b">
 
                     <h1 className="titulo_pagina_configuracoes">NEWSLETTER</h1>
+                    <img src={IMAGEN} alt="Em construção" />
 
                     <h2>Conteudo aqui</h2>
                     
