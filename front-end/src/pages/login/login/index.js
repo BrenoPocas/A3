d8ffd5128c53c32d95197f9480168af846ae1bfc
Marcello Júnior <celojuniorii2003@gmail.com>
2024-11-04 23:18:58 -0300

    teste-breno-ver

--- front-end/src/pages/login/login/index.js
+++ front-end/src/pages/login/login/index.js
@@ -16,25 +16,33 @@ function Login() {
 
         <section>
 
-        <form className="formulario_login" action="#">
+            <div className="container">
 
+        <form className="formulario_login" action="#">
+            
+            <h1>Faça seu login</h1> 
+        <div className="email-senha">
             <label for="email">E-mail:</label>
             <input type="email" id="email-login" required/>
-
+     </div>
+     <div className="email-senha">
             <label for="pass-login">Senha:</label>
             <input type="password" id="pass" name="password" minlength="8" required/>
+     </div>     
+            <button className="botao">Entrar</button>
 
+            <div className="recall-forget">
             <ul>
                 <li className="link_login"><button className="botao_link" onClick={() => navigateTo('/login/esqueceu-senha')}>Esqueceu a senha?</button></li>
-            </ul>          
-
-            <button className="botao_entrar" type="button">Entrar</button>
+            </ul> 
+            </div>    
 
             <ul>
-                <li className="link_login"><button className="botao_link" onClick={() => navigateTo('/login/criar-conta')}>Ainda sem conta? Clique aqui!</button></li>
+                <li className="link_login"><button className="botao_link" onClick={() => navigateTo('/login/criar-conta')}>Ainda sem conta?  Clique aqui!</button></li>
             </ul> 
 
         </form>
+        </div>
 
         </section>
 
