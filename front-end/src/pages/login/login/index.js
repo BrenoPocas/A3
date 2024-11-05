974c674d1276bc9985550ad95ae0dd1106fcfb48
BrenoPocas <brenomouraopocas@gmail.com>
2024-11-05 01:04:49 -0300

    css form login

--- front-end/src/pages/login/login/index.js
+++ front-end/src/pages/login/login/index.js
@@ -6,47 +6,44 @@ import { useNavigate } from "react-router-dom"
 function Login() {
 
     const navigate = useNavigate()
-    function navigateTo(destination) {  
-      navigate(destination)
+    function navigateTo(destination) {
+        navigate(destination)
     }
 
     return <div>
-        
-        <Header/>
 
-        <section>
+        <Header />
 
-            <div className="container">
+        <section className="container">
 
-        <form className="formulario_login" action="#">
-            
-            <h1>Faça seu login</h1> 
-        <div className="email-senha">
-            <label for="email">E-mail:</label>
-            <input type="email" id="email-login" required/>
-     </div>
-     <div className="email-senha">
-            <label for="pass-login">Senha:</label>
-            <input type="password" id="pass" name="password" minlength="8" required/>
-     </div>     
-            <button className="botao">Entrar</button>
+                <form className="formulario_login" action="#">
 
-            <div className="recall-forget">
-            <ul>
-                <li className="link_login"><button className="botao_link" onClick={() => navigateTo('/login/esqueceu-senha')}>Esqueceu a senha?</button></li>
-            </ul> 
-            </div>    
+                    <h1>Faça seu login</h1>
+                    <div className="email-senha">
+                        <label for="email">E-mail:</label>
+                        <input type="email" id="email-login" required />
+                    </div>
+                    <div className="email-senha">
+                        <label for="pass-login">Senha:</label>
+                        <input type="password" id="pass" name="password" minlength="8" required />
+                    </div>
+                    <button className="botao_login">Entrar</button>
 
-            <ul>
-                <li className="link_login"><button className="botao_link" onClick={() => navigateTo('/login/criar-conta')}>Ainda sem conta?  Clique aqui!</button></li>
-            </ul> 
+                    <div className="recall-forget">
+                        <ul>
+                            <li className="link_login"><button className="botao_link" onClick={() => navigateTo('/login/esqueceu-senha')}>Esqueceu a senha?</button></li>
+                        </ul>
+                    </div>
 
-        </form>
-        </div>
+                    <ul>
+                        <li className="link_login"><button className="botao_link" onClick={() => navigateTo('/login/criar-conta')}>Ainda sem conta?  Clique aqui!</button></li>
+                    </ul>
+
+                </form>
 
         </section>
 
-        <Footer/>
+        <Footer />
 
     </div>;
 }
