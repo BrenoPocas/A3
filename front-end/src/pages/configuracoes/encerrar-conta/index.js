7c50be9db69d8980c4230c939f24a36e5369cf1c
Marcello Júnior <celojuniorii2003@gmail.com>
2024-11-06 22:34:07 -0300

    css-encerrar-conta

--- front-end/src/pages/configuracoes/encerrar-conta/index.js
+++ front-end/src/pages/configuracoes/encerrar-conta/index.js
@@ -13,11 +13,11 @@ function EncerrarConta() {
 
                 <ItensConfiguracoes/>
 
-                <div className="encerrar conta">
+                <div className="encerrar-conta">
 
                     <h1 className="titulo_pagina_configuracoes">ENCERRAR CONTA</h1>
 
-                    <h4> Se você encerrar sua conta, terá a inscrição cancelada em todos os trabalhos voluntários e perderá acesso à sua conta e todos os dados associados a ela para sempre, mesmo se você criar uma nova conta usando o mesmo endereço de e-mail no futuro. </h4>
+                    <h4 className='info'> Se você encerrar sua conta, terá a inscrição cancelada em todos os trabalhos voluntários e perderá acesso à sua conta e todos os dados associados a ela para sempre, mesmo se você criar uma nova conta usando o mesmo endereço de e-mail no futuro. </h4>
 
                     <button className="botao_encerrar_conta" type="button">Encerrar Conta</button>
                     
